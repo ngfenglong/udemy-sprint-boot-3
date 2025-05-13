1e3bc5563ec52b92e1de7093d3abc485ac7c0bbb
Zell <fenglong1502@hotmail.com>
2025-05-13 22:40:31 +0800

    Setup API Exception using Controller Advice

---
+++ 04-Rest-CRUD-APIs/demo/src/main/java/com/luv2code/demo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.luv2code.demo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        StudentErrorResponse err = new StudentErrorResponse();
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setMessage(exc.getMessage());
+        err.setTimeStamp(System.currentTimeMillis());
+
+
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        StudentErrorResponse err = new StudentErrorResponse();
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setMessage(exc.getMessage());
+        err.setTimeStamp(System.currentTimeMillis());
+
+
+        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+    }
+
+}
