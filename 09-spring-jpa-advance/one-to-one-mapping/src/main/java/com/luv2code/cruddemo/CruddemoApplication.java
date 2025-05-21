e9dec26a14e85e8a272ae0958dfba0430ec81f97
Zell <fenglong1502@hotmail.com>
2025-05-21 08:50:34 +0800

    JPA OneToOne OneDirectional mapping

--- 09-spring-jpa-advance/one-to-one-mapping/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-jpa-advance/one-to-one-mapping/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,7 +1,12 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +15,49 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDao){
+		return runner -> {
+			// createInstructor(appDao);
+			// findInstructor(appDao);
+			deleteInstructor(appDao);
+		};
+	}
+
+	private void deleteInstructor(AppDAO appDao) {
+		int theId = 1;
+		System.out.println("Deleting instructor id: " + theId);
+
+		appDao.deleteInstructorById(theId);
+		System.out.println("Done");
+	}
+
+	private void findInstructor(AppDAO appDao) {
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDao.findInstructorById(theId);
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDao) {
+//		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@lub2code.com");
+//
+//		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube,.com", "Luv 2 code !!!");
+
+
+		Instructor tempInstructor = new Instructor("Madhu", "Patel", "Patel@lub2code.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube,.com", "Guitar");
+
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor " + tempInstructor);
+		appDao.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
 }
